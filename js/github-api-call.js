e45ecf8fb8eb54e782527c29b02350ab3e272426
Travis Hohl <travis@travishohl.com>
2016-02-09 20:16:35 -0600

    Complete rewrite of old site's coffee script.
    Next step: use local storage to cache the response.

--- js/github-api-call.js
+++ js/github-api-call.js
@@ -1,76 +1,69 @@
 // When the DOM is loaded, but before all page assets are loaded.
 document.addEventListener("DOMContentLoaded", function(event) {
 
-    function reqListener() {
+    function request_listener() {
 
         console.log(this);
 
         // Get components to "Latest Push" box.
-        var avatar = document.getElementById("avatar");
-        var date = document.getElementById("date");
-        var action = document.getElementById("action");
+        var avatar_element = document.getElementById("avatar");
+        var date_element = document.getElementById("date");
+        var action_element = document.getElementById("action");
 
         // Grab the first (latest) event and parse as Json.
         var response_json = JSON.parse(this.response)[0];
 
-        // Create anchor.
+        // Format event date and add it to the date element.
+        var date_value = new Date(response_json.created_at);
+        date_value = date_value.toLocaleString();
+        date_text_node = document.createTextNode(date_value);
+        date_element.appendChild(date_text_node);
+
+        // Add src attribute to avatar image element.
+        avatar_element.src = response_json.actor.avatar_url;
+
+        // Assemble action string -- create link to GitHub user.
         var anchor = document.createElement("a");
         anchor.href = "https://github.com/" + response_json.actor.login;
         anchor.target = "_blank";
 
-        // Put text within anchor.
+        // Assemble action string -- put text within link.
         var name = document.createTextNode(response_json.actor.login);
         anchor.appendChild(name);
 
-        // Put anchor within "action" dialogue area.
-        action.appendChild(anchor);
+        // Assemble action string -- put link within "action" dialogue area.
+        action_element.appendChild(anchor);
 
-        // Add src attribute to avatar image element.
-        avatar.src = response_json.actor.avatar_url;
-
-        // Assemble action string.
+        // Assemble action string -- associate verbs with events.
         var type = response_json.type;
         var verbs = { PushEvent: "pushed" };
         var type_as_verb = verbs[type];
-        var action_string = document.createTextNode(" " + type_as_verb);
-        action.appendChild(action_string);
+
+        // Assemble action string -- get payload size and commit or commits?
+        var payload_size = response_json.payload.size;
+        var commit_s = (payload_size > 1) ? "commits" : "commit";
+
+        // Assemble action string.
+        var action_string = document.createTextNode(" " + type_as_verb + " " + payload_size + " " + commit_s + " to ");
+        action_element.appendChild(action_string);
+
+        // Assemble action string -- create link to GitHub repo.
+        var anchor = document.createElement("a");
+        anchor.href = "https://github.com/" + response_json.repo.name;
+        anchor.target = "_blank";
+
+        // Assemble action string -- put text within link.
+        var name = document.createTextNode(response_json.repo.name);
+        anchor.appendChild(name);
+
+        // Assemble action string -- put link within "action" dialogue area.
+        action_element.appendChild(anchor);
     }
 
     // Create a new XMLHttpRequest and send it to GitHub.
     var request = new XMLHttpRequest();
-    request.addEventListener("load", reqListener);
+    request.addEventListener("load", request_listener);
     request.open("GET", "https://api.github.com/users/travishohl/events");
     request.setRequestHeader("Accept", "application/vnd.github.v3+json");
     request.send();
 });
-
-/*
-insert_response_into_dom = (response) ->
-	# Find the latest "PushEvent"
-	i = 0
-	i++ while response[i].type isnt "PushEvent"
-	latest_event = response[i]
-
-	# Pick out and massage data from GitHub
-	date = new Date(latest_event.created_at)
-	date = date.toLocaleString()
-	actor_name = latest_event.actor.login
-	avatar_url = latest_event.actor.avatar_url
-
-	type = latest_event.type
-	verbs = { PushEvent: "pushed" }
-	type = verbs[type] # Convert the "type" to a human-readable verb
-
-	payload_size = latest_event.payload.size
-	commit_s = if (payload_size > 1) then "commits" else "commit" # good grammar
-	repo_name = latest_event.repo.name
-
-	# Generate action string
-	action = "<a href=\"https://github.com/#{actor_name}\" target=\"_blank\">#{actor_name}</a> #{type} #{payload_size} #{commit_s} to <a href=\"https://github.com/#{repo_name}\" target=\"_blank\">#{repo_name}</a>."
-
-	# Insert the latest_event into the DOM
-	jQuery("div.github_feed img#avatar").attr src: avatar_url
-	jQuery("div.github_feed small#date").html date
-	jQuery("div.github_feed div#action").html action
-	return
-*/
